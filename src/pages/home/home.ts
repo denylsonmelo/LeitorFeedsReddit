7acd5cde85356d68e6b4b40c45f538f9d0ce3890
Denylson Melo <denylsonmelo@gmail.com>
2017-02-24 00:06:27 -0300

    incluindo imagem padrao no sitens sem imagem

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,7 +10,7 @@ import { InAppBrowser } from 'ionic-native'
 })
 export class HomePage {
 
-  public feeds: Array<string>;
+  public feeds: Array<any>;
   private url: string = "https://www.reddit.com/new.json";
 
   constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
@@ -31,6 +31,13 @@ export class HomePage {
     this.http.get(this.url).map(res => res.json())
       .subscribe(data => {
         this.feeds = data.data.children;
+
+        this.feeds.forEach((e, i, a) => {
+          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) {
+            e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+          }
+        })
+
         loading.dismiss();
       });
   }
