ab87050947581a67bf6bbc038b552e052fdaf00e
Denylson Melo <denylsonmelo@gmail.com>
2017-02-24 00:18:59 -0300

    adicionando scroll infinito

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,9 +12,27 @@ export class HomePage {
 
   public feeds: Array<any>;
   private url: string = "https://www.reddit.com/new.json";
+  private olderPosts: string = "https://www.reddit.com/new.json?after=";
 
   constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
     this.fetchContent();
+  }
+
+  doInfinite(infiniteScroll){
+    let paramsUrl = (this.feeds.length > 0 ) ? this.feeds[this.feeds.length -1].data.name : "";
+
+    this.http.get(this.olderPosts + paramsUrl).map(res => res.json())
+      .subscribe(data => {
+        this.feeds = this.feeds.concat(data.data.children);
+
+        this.feeds.forEach((e, i, a) => {
+          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) {
+            e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+          }
+
+        })
+        infiniteScroll.complete();
+      })
   }
 
   itemSelected(url: string):void{
